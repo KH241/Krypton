1fa0774f5484040e563b92b2dbd8c2c09c2eae95
KH241 <k.hurt@gmx.de>
2021-11-27 03:18:06 +0100

    updated doxygen

--- Krypton/Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Krypton/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MainMenuScript : MonoBehaviour
 {
+    /**
+     * Exits the Application - called from the MainMenu-Button "Exit"
+     */
     public void OnExit()
     {
         Application.Quit();
