ec36983063f86292efefc7fd04c96202e931b0d8
KH241 <k.hurt@gmx.de>
2021-12-02 23:31:34 +0100

    ugly but functional Main Menu

--- Krypton/Assets/Scripts/MainMenu/MainMenuScript.cs
+++ Krypton/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -1,9 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+	/**
+     * Opens the Atom-View - called from the MainMenu-Button "Atom"
+     */
+	public void OnAtom()
+	{
+		SceneManager.LoadScene(SceneList.AtomView);
+	}
+
+	/**
+     * Opens the Molecule-View - called from the MainMenu-Button "Molecule"
+     */
+	public void OnMolecule()
+	{
+		SceneManager.LoadScene(SceneList.MoleculeView);
+	}
+
+	/**
+     * Opens the Task-Menu - called from the MainMenu-Button "Task"
+     */
+	public void OnTask()
+	{
+		SceneManager.LoadScene(SceneList.TaskMenu);
+	}
+
+	/**
+     * Opens the Help-Scene - called from the MainMenu-Button "Help"
+     */
+	public void OnHelp()
+	{
+		SceneManager.LoadScene(SceneList.Help);
+	}
+    
     /**
      * Exits the Application - called from the MainMenu-Button "Exit"
      */
---
+++ Krypton/Assets/Scripts/Utility/SceneList.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public static class SceneList
+{
+	public static string MainMenu = "MainMenuScene";
+	public static string AtomView = "AtomScene";
+	public static string MoleculeView = "MoleculeScene";
+	public static string TaskMenu = "TaskMenuScene";
+	public static string Help = "HelpScene";
+}
