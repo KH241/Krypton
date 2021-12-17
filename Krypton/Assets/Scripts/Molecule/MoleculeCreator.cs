127f43f7d27bb2ad0192b5d5b63a46b70eb1158d
Youssef.Nassar <yossef41997@gmail.com>
2021-12-17 11:51:54 +0100

    scan scene for imageTargets

--- Krypton/Assets/Scripts/Molecule/MoleculeCreator.cs
+++ Krypton/Assets/Scripts/Molecule/MoleculeCreator.cs
@@ -1,44 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
 public class MoleculeCreator : MonoBehaviour
 {
-    public GameObject atom1;
-    public GameObject atom2;
-    public GameObject atom3;
+	public GameObject atom1;
+	public GameObject atom2;
+	public GameObject atom3;
 
-    public GameObject mole;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject mole;
+	private GameObject[] _gameObjects;
 
-    // Update is called once per frame
-    void Update()
-    {
-        float distance12 = Vector3.Distance(atom1.transform.position, atom2.transform.position);
-        float distance13 = Vector3.Distance(atom1.transform.position, atom3.transform.position);
-        float distance23 = Vector3.Distance(atom2.transform.position, atom3.transform.position);
-        float valid1 = distance12 / 10;
-        float valid2 = distance13 / 10;
-        float valid3 = distance23 / 10;
-        
-        
-        mole.SetActive(false);
-        
-        if (distance12 > 1) {
-			
-            mole.SetActive(false);
-        }
-        else if (0.2 > distance12 && 0.2 > distance13 && 0.2 > distance23)
-        {
-            mole.SetActive(true);
-        }
-     
-        Debug.Log (distance12);
-        Debug.Log (distance13);
-        Debug.Log (distance23);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		// _gameObjects = GameObject.FindGameObjectsWithTag("Molecule");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+		_gameObjects =  GameObject.FindGameObjectsWithTag("ImageTarget");
+
+		
+		for (int i = 0; i < _gameObjects.Length; i++)
+		{
+			var trackable = _gameObjects[i].GetComponent<TrackableBehaviour>();
+			var status = trackable.CurrentStatus;
+			Debug.Log(_gameObjects[i].name + status);
+		}
+
+		sleep();
+		
+		///////////////////////////////////////////////
+		float distance12 = Vector3.Distance(atom1.transform.position, atom2.transform.position);
+		float distance13 = Vector3.Distance(atom1.transform.position, atom3.transform.position);
+		// float distance23 = Vector3.Distance(atom2.transform.position, atom3.transform.position);
+		float valid1 = distance12 / 10;
+		float valid2 = distance13 / 10;
+		// float valid3 = distance23 / 10;
+
+
+		mole.SetActive(false);
+
+		if (distance12 > 7)
+		{
+			mole.SetActive(false);
+		}
+		else if (5 > distance12 && 5 > distance13 ) //&& 5 > distance23)
+		{
+			mole.SetActive(true);
+		}
+
+		Debug.Log("distance12: " + distance12);
+		Debug.Log("distance13: " + distance13);
+		// Debug.Log("distance23" + distance23);
+	}
+
+	public IEnumerable<WaitForSeconds> sleep()
+	{
+		yield return new WaitForSeconds(1);
+	}
 }
