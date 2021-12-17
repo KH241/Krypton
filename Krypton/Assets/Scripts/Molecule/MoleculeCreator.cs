b60c8096ce4c3ab8ebc5727435cceef30cfd4756
Youssef.Nassar <yossef41997@gmail.com>
2021-12-17 13:03:38 +0100

    try to spawn atoms

--- Krypton/Assets/Scripts/Molecule/MoleculeCreator.cs
+++ Krypton/Assets/Scripts/Molecule/MoleculeCreator.cs
@@ -11,25 +11,42 @@ public class MoleculeCreator : MonoBehaviour
 
 	public GameObject mole;
 	private GameObject[] _gameObjects;
+	private AtomManager _atomManager;
+	public AtomSO oxygenData;
+	public AtomSO hydrogenData;
+	
+	public GameObject atomPrefab;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		// _gameObjects = GameObject.FindGameObjectsWithTag("Molecule");
+		_atomManager = gameObject.AddComponent<AtomManager>();
+		_atomManager.atomPrefab = atomPrefab;
+		_atomManager.hydrogenData = hydrogenData;
+		_atomManager.oxygenData = oxygenData;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		
 		_gameObjects =  GameObject.FindGameObjectsWithTag("ImageTarget");
-
 		
 		for (int i = 0; i < _gameObjects.Length; i++)
 		{
 			var trackable = _gameObjects[i].GetComponent<TrackableBehaviour>();
 			var status = trackable.CurrentStatus;
 			Debug.Log(_gameObjects[i].name + status);
+		}
+
+		for (int i = 0; i < _gameObjects.Length; i++)
+		{
+			var trackable = _gameObjects[i].GetComponent<TrackableBehaviour>();
+			var status = trackable.CurrentStatus;
+			Debug.Log(_gameObjects[i].name + status);
+			if (status == Status.TRACKED)
+			{
+				SpawnAtom(_gameObjects[i]);
+			}
 		}
 
 		sleep();
@@ -57,6 +74,11 @@ public class MoleculeCreator : MonoBehaviour
 		Debug.Log("distance12: " + distance12);
 		Debug.Log("distance13: " + distance13);
 		// Debug.Log("distance23" + distance23);
+	}
+
+	private void SpawnAtom(GameObject atom)
+	{
+		_atomManager.createAtom(hydrogenData.ToString(), atom.transform.position);
 	}
 
 	public IEnumerable<WaitForSeconds> sleep()
